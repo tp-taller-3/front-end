c5671d75ee191a96b08c99531d3d823e374cca4b
Dylan Alvarez <dylan@edenworkplace.com>
2022-12-04 23:18:49 -0300

    Sort questions

--- src/pages/Applicant/SurveyResult/ResultsComponent/component.tsx
+++ src/pages/Applicant/SurveyResult/ResultsComponent/component.tsx
@@ -6,21 +6,24 @@ import { PaginationTable } from "../PaginationTable";
 import { Card } from "../../../../components/Card";
 
 import styles from "./styles.module.scss";
+import { sortBy } from "lodash";
 
 export const ResultsComponent: FunctionComponent<ICourse> = ({ course }) => {
   const questions = useQuestions(course);
 
   return (
     <>
-      {questions?.map(question => (
-        <Card className={styles.questionContainer} key={question.uuid}>
-          {question.isPublic ? (
-            <AnswerComponent question={question} />
-          ) : (
-            <PaginationTable question={question} headers={["Respuesta", "# Ocurrencias"]} />
-          )}
-        </Card>
-      ))}
+      {sortBy(questions || [], ["isPublic", "category", "questionText", "teacherName"]).map(
+        question => (
+          <Card className={styles.questionContainer} key={question.uuid}>
+            {question.isPublic ? (
+              <AnswerComponent question={question} />
+            ) : (
+              <PaginationTable question={question} headers={["Respuesta", "# Ocurrencias"]} />
+            )}
+          </Card>
+        )
+      )}
     </>
   );
 };
